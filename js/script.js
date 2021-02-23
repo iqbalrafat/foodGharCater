e2a6f17051464f0163eda1990199d9efc4af534f
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-23 00:27:51 -0700

    [foodGhar]--update JavaScript

--- js/script.js
+++ js/script.js
@@ -12,12 +12,21 @@ $(window).scroll(()=>{
     $('.navbar').removeClass('fixed-top');
   }
 })
-
-
-
-
-
-
+//smooth scroll
+$('.nav-item a, .header-link,#back-to-top').click(function(link){
+  link.preventDefault();
+  let target=$(this).attr('href');
+  $('html,body').stop().animate({scrollTop:$(target).offset().top -25},3000);
+})
+$(window).scroll(()=>{
+  let position=$(this).scrollTop();  // here we are taking the value of the position of window and assign it to position.
+  if(position>=718){
+    $('#back-to-top').addClass('scrollTop');
+    
+  } else{
+    $('#back-to-top').removeClass('scrollTop');    
+  }
+})
 
 $(document).ready(function(){
 //  added ripple effect
